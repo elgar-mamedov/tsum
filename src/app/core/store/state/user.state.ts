21e113602f67b37fce86edb1a3866cb5bdcbc276
Elgar Mamedov <elgar.mamedov@rightbalance.io>
2020-08-19 11:53:27 +0300

    removed stub data from store

--- src/app/core/store/state/user.state.ts
+++ src/app/core/store/state/user.state.ts
@@ -5,27 +5,5 @@ export interface IUserState {
 }
 
 export const initialUserState: IUserState = {
-  // users: [],
-  users: [
-    {
-      id: 1,
-      firstName: 'Иванов Иван Иванович',
-      gender: 'male',
-      materialStatus: 'married',
-      amountOfChildren: 2,
-      email: 'ivan@ivan',
-      comment: 'комент',
-      dateOfBirth: '1/1/1990'
-    },
-    {
-      id: 2,
-      firstName: 'Иванова Ольга Андреевна',
-      gender: 'female',
-      materialStatus: 'married',
-      amountOfChildren: 1,
-      email: 'masha@masha',
-      comment: 'комент',
-      dateOfBirth: '1/1/1997'
-    }
-  ],
+  users: [],
 };
